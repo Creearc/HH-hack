b5171bbf2037ea34df47295503132f5ea025ae97
Alexandr <knxandr@rambler.ru>
2022-02-11 21:53:43 +0500

    Update calc_sum.py

--- jupiter/calc_sum.py
+++ jupiter/calc_sum.py
@@ -5,10 +5,13 @@ arr = ''
 csvfile = open('train_00.csv', 'r', encoding='utf-8')
 f = csv.reader(csvfile, delimiter=',')
 mx = 0
+tmp = ''
 for s in f:
   l = len(s[0].split())
   if l > mx:
     mx = l
+    tmp = s[0] 
 #f.close()
 
 print(mx)
+print(tmp)
